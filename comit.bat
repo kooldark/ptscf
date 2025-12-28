602d4d9f14535f27f9e886bf0838cfc043c8bd68
Tuấn KoolD <tr.nhutuan@gmail.com>
2025-12-28 14:56:23 +0700

    update

--- comit.bat
+++ comit.bat
@@ -1,3 +1,17 @@
-git add .
-git commit
+@echo off
+setlocal enabledelayedexpansion
+
+:: Commit message nếu người dùng nhập vào
+set msg=%*
+if "%msg%"=="" set msg=update
+
+echo ===========================
+echo Git Auto Commit & Push
+echo ===========================
+
+git add -A
+git commit -m "%msg%"
 git push
+
+echo Done.
+pause
